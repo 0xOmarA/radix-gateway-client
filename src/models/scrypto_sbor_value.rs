d9c15757e9e1029bce9c0f4ca13f41f6401beda0
Omar <OmarAbdulla7@hotmail.com>
2024-03-23 21:34:43 +0300

    convert programmatic json field to serde_json value

--- src/models/scrypto_sbor_value.rs
+++ src/models/scrypto_sbor_value.rs
@@ -3,14 +3,11 @@ pub struct ScryptoSborValue {
     #[serde(rename = "raw_hex")]
     pub raw_hex: String,
     #[serde(rename = "programmatic_json")]
-    pub programmatic_json: Box<crate::models::ProgrammaticScryptoSborValue>,
+    pub programmatic_json: Box<serde_json::Value>,
 }
 
 impl ScryptoSborValue {
-    pub fn new(
-        raw_hex: String,
-        programmatic_json: crate::models::ProgrammaticScryptoSborValue,
-    ) -> ScryptoSborValue {
+    pub fn new(raw_hex: String, programmatic_json: serde_json::Value) -> ScryptoSborValue {
         ScryptoSborValue {
             raw_hex,
             programmatic_json: Box::new(programmatic_json),
